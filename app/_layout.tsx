e78276c623fc2529dc45578be67499fdf53652d1
zen-daiki <d.yokoki@zenplace.co.jp>
2025-01-18 19:06:56 +0900

    edit: Configure status bar, splash screen and app icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
+
+import { StatusBar } from "expo-status-bar";
+
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
