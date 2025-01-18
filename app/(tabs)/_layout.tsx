dda1091e331cb8d19700c7ec2e4f093f291c8ed5
zen-daiki <d.yokoki@zenplace.co.jp>
2025-01-18 11:58:38 +0900

    add: navigationタブの追加

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="about" options={{ title: 'About' }} />
+    </Tabs>
+  );
+}
