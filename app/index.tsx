5cdfad539cb11999a8c51e062febe4ae726f5146
zen-daiki <d.yokoki@zenplace.co.jp>
2025-01-18 10:58:34 +0900

    edit: reset-projectとaboutの追加

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+
+import { Text, View,  StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
