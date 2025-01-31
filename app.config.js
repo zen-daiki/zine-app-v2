7164658436789e63bf9d3376c2f7bfe22c4c69ce
zen-daiki <d.yokoki@zenplace.co.jp>
2025-01-31 10:18:28 +0900

    feat: add microCMS configuration and client setup

---
+++ app.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  expo: {
+    name: "zien-app",
+    extra: {
+      microCmsServiceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+      microCmsApiKey: process.env.MICROCMS_API_KEY,
+    },
+  },
+};
