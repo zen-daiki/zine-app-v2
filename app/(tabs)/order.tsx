24d4d5195342773c713b04ff40894833591b2161
zen-daiki <d.yokoki@zenplace.co.jp>
2025-02-15 23:16:35 +0900

    refactor: 注文履歴画面をシンプルに修正

--- app/(tabs)/order.tsx
+++ app/(tabs)/order.tsx
@@ -1,15 +1,9 @@
-import { Stack } from 'expo-router';
 import { Text, View, StyleSheet } from 'react-native';
 
 export default function OrderScreen() {
   return (
     <View style={styles.container}>
-      <Stack.Screen
-        options={{
-          title: 'ご注文履歴',
-        }}
-      />
-      <Text style={styles.text}>Order screen</Text>
+      <Text style={styles.text}>注文履歴</Text>
     </View>
   );
 }
